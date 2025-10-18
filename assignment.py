3f11efea323f95a75880a6cd23f5a7c414fcff7c
kieron <ksiriban@gmail.com>
2025-10-18 14:44:58 +0800

    assignment 1.1

--- assignment.py
+++ assignment.py
@@ -17,11 +17,33 @@ def fizz_buzz(number):
     """
     return
 
+# assignemnt 1.1 Q1
+
+def fizz_buzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 5 == 0:
+        return "Buzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    else:
+        return number
+
+number = 9
+print (fizz_buzz(number))
+number = 10
+print (fizz_buzz(number))
+number = 10
+print (fizz_buzz(number))
+number = 16
+print (fizz_buzz(number))
+
 
 # Question 2
 
 # Write a function that takes a list of numbers and returns the sum of the squares of all the numbers.
 
+# assignemnt 1.1 Q2
 
 def sum_of_squares(numbers):
     """Returns the sum of the squares of all the numbers in a list.
@@ -31,6 +53,15 @@ def sum_of_squares(numbers):
     56
     """
     return
+
+def sum_of_squares(numbers):
+   return sum([number**2 for number in numbers])
+
+numbers = [1 ,2 ,3]
+print (sum_of_squares(numbers))
+
+numbers = [2 ,4 ,6]
+print (sum_of_squares(numbers))
 
 
 # Question 3
@@ -48,6 +79,20 @@ def count_vowels(string):
     return
 
 
+# assignemnt 1.1 Q3
+
+def count_vowels(string):
+    vowels = "aeiou"
+    return len([word for word in string if word in vowels])
+
+
+string = "hello"
+print (count_vowels(string))
+string = "aeiou"
+print (count_vowels(string))
+string = "kierun"
+print (count_vowels(string))
+
 # Question 4
 
 # Write a function that counts the number of repeated characters in a string.
@@ -61,6 +106,18 @@ def count_repeats(string):
     0
     """
     return
+
+# assignemnt 1.1 Q4
+
+def count_repeats(string):
+    return len([word for word in string if string.count(word) > 1])
+
+string = "hello"
+print (count_repeats(string))
+string = "aeiou"
+print (count_repeats(string))
+string = "Kiekie"
+print (count_repeats(string))
 
 
 if __name__ == "__main__":
